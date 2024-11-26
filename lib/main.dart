0c24fdd3d768aeb1ee96a91cbd33c4c62a520cb0
Too Yun Jie <too.yun.j@gmail.com>
2024-11-26 10:51:21 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("LOGIN"),
+        backgroundColor: Colors.blue[600],
+        centerTitle: true,
+      ),
+      body: const Home(),
+    ),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hi everyone. my name is gay");
+  }
+}
