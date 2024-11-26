caabd1cb995c52ec99111adde9e13a399d093ae8
Too Yun Jie <too.yun.j@gmail.com>
2024-11-26 15:12:16 +0800

    Implement with google map API

---
+++ lib/home/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Text("53100000"),
+      bottomNavigationBar: BottomAppBar(
+        color: const Color.fromARGB(255, 35, 152, 255),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ElevatedButton(onPressed: (){}, style: ElevatedButton.styleFrom(shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))), child: const Text("Explore")),
+            ElevatedButton(onPressed: (){}, style: ElevatedButton.styleFrom(shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))), child: const Text("Route")),
+            ElevatedButton(onPressed: (){}, style: ElevatedButton.styleFrom(shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))), child: const Text("Info")),
+            ElevatedButton(onPressed: (){}, style: ElevatedButton.styleFrom(shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))), child: const Text("Profile")),
+          ]
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:smart_transportation/home/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("LOGIN"),
-        backgroundColor: Colors.blue[600],
-        centerTitle: true,
-      ),
-      body: const Home(),
-    ),
+  runApp(const MaterialApp(
+    home: Home(),
   ));
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text("Test");
-  }
-}
